648bfaca93d8e592f490f209b3e9ba00756afc63
shrLaksh15 <shrlakshmanan@gmail.com>
2026-09-19 20:29:52 -0700

    Add files via upload

---
+++ InformationMsg.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class InformationMsg {
+    public static void main(String[] args) {
+    
+        JOptionPane.showMessageDialog(
+            null,                                
+            "This is an information message!", 
+            "Information Message. ",                
+            JOptionPane.INFORMATION_MESSAGE      
+        );
+        
+    }
+}
+
